e015da5343ef6fcaafe2a48b54c18e56b1888cdc
Kozma Márton <kozmamarton76@gmail.com>
2024-10-23 18:46:52 +0200

    Implemented bare minimum usable client interface

--- GameSrc/src/Client/Client.cpp
+++ GameSrc/src/Client/Client.cpp
@@ -1,3 +1,78 @@
 #include "Client.hpp"
+#include "../Utility/logging.hpp"
+#include <thread>
+
+sf::Time Client::connTimeoutSec = sf::seconds(20);
+unsigned short Client::timeoutMs = 30;
+unsigned short Client::maxRetries = 3;
 
 
+Client::Client(const std::string& address, uint16_t port): port(port) {
+	connTimeoutSec = sf::seconds(20);
+	serverAddress = sf::IpAddress(address.c_str());
+}
+
+sf::Socket::Status Client::connectToServer(sf::Time timeout ) 
+{
+	return Server.connect(this->serverAddress, this->port);
+}
+
+
+// Getters
+sf::IpAddress Client::getServerAddress() const {
+    return serverAddress;
+}
+
+uint16_t Client::getPort() const {
+    return port;
+}
+
+sf::TcpSocket& Client::getServer() {
+    return Server;
+}
+
+sf::Time Client::getConnTimeoutSec() {
+    return connTimeoutSec;
+}
+
+// Setters
+
+void Client::setServerAddress(const sf::IpAddress& address) {
+    serverAddress = address;
+}
+
+void Client::setPort(uint16_t newPort) {
+    port = newPort;
+}
+
+void Client::setConnTimeoutSec(unsigned short newTimeout) {
+    connTimeoutSec = sf::seconds(newTimeout);
+}
+
+bool Client::msgToServer(sf::Packet &msg) {
+    if (getServer().NotReady) {
+        return false;
+    }
+    short attempts = 0;
+    while (attempts < Client::maxRetries) {
+        if (getServer().send(msg) != sf::Socket::Partial) {
+            break;
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(Client::timeoutMs));
+        attempts++;
+    }
+    return attempts != Client::maxRetries;
+
+}
+
+sf::Packet Client::msgFromServer(unsigned short timeout) {
+    auto response = sf::Packet();
+    try {
+        this->getServer().receive(response);
+    }
+    catch (std::exception e) {
+        logErr("An error occurred during recieving from server!");
+    }
+    return response;
+
+}
--- GameSrc/src/Client/Client.hpp
+++ GameSrc/src/Client/Client.hpp
@@ -3,15 +3,30 @@
 
 class Client {
 	sf::TcpSocket Server;
-	sf::IpAddress serverAdress;
-public:
-	static sf::Time timeoutSec;
+	sf::IpAddress serverAddress;
+	uint16_t port;
+protected:
+    sf::TcpSocket& getServer();
 
-	Client(const std::string& adress, uint16_t port) {
-		timeoutSec = sf::seconds(20);
-		serverAdress = sf::IpAddress(adress.c_str());
-		Server.connect(serverAdress,port,timeoutSec);
-	}
+public:
+	static sf::Time connTimeoutSec;
+	static unsigned short maxRetries;
+	static unsigned short timeoutMs;
+
+	Client(const std::string& address, uint16_t port);
+	~Client() = default;
+
+    sf::IpAddress getServerAddress() const;
+    uint16_t getPort() const;
+    static sf::Time getConnTimeoutSec();
+	
+    void setServerAddress(const sf::IpAddress& address);
+    void setPort(uint16_t newPort);
+    static void setConnTimeoutSec(unsigned short newTimeout);
+	sf::Socket::Status connectToServer(sf::Time timeout = Client::connTimeoutSec);
+
+	bool msgToServer(sf::Packet &msg);
+	sf::Packet msgFromServer(unsigned short timeOut = timeoutMs);
 
 
 };
