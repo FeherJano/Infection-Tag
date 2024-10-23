e1d1169597dc8826a9c859b1d088c063c91d141d
Kozma Márton <kozmamarton76@gmail.com>
2024-10-23 18:52:04 +0200

    added loglevels, and std logging

--- GameSrc/src/Utility/logging.hpp
+++ GameSrc/src/Utility/logging.hpp
@@ -1,6 +1,22 @@
 #pragma once
 #include <iostream>
+#include <chrono>
+#include <iomanip>
+#include <ctime>
 #include "Exceptions.hpp"
 
+
+//TODO implement time getter function
+static const char* getTime() {
+	return "";
+}
+
+#define log(msg,level) std::cout<< getTime <<" |"<<level <<"| : "<<msg;
 #define logErr(msg) std::cerr<<"An error occurred in: "<<__FILE__<<" at: "<< __LINE__ <<"\n Err message: \n"<<msg;
-constexpr auto err_Fatal = -1;
+
+constexpr int err_Fatal = -1;
+const std::string loggingInfo = "INFO";
+const std::string loggingWarn = "WARNING";
+const std::string loggingErr = "ERROR";
+
+
